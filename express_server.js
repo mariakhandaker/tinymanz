0a0a1dd2f7d3ee0f82d5a956e608f97bb14b0eea
Maria Khandaker <mariakhandaker@Marias-MacBook-Pro.local>
2020-08-31 18:20:52 -0400

    generateRandomString function added

--- express_server.js
+++ express_server.js
@@ -10,6 +10,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(3, 9);
+}
+
 app.get("/", (req, res) => {
   // res.render("");
 });
