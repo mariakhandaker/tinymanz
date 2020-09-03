0747b8f8ce34e9e593262595da481d6fdffb5742
Maria Khandaker <mariakhandaker@Marias-MacBook-Pro.local>
2020-09-03 18:36:33 -0400

    added cookie session, removed cookie parser

--- express_server.js
+++ express_server.js
@@ -4,15 +4,22 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
+const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 const { doesUserExist, letUserLogin, urlsForUser } = require('./helpers');
 
 //setting middlewares for use
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: [/* secret keys */],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
